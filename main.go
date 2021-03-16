cca87872a96d84d2025546f0d7d30ce73aa431f3
Danial Pearce <danial.pearce@rea-group.com>
2021-03-16 14:05:18 +1100

    According to semver.org we are ready for 1.5.0

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "shush"
-	app.Version = "1.4.1-with-new-aws-sdk"
+	app.Version = "1.5.0"
 	app.Usage = "KMS encryption and decryption"
 
 	app.Flags = []cli.Flag{
